9fc657b491f8a2d4ea6857f4201564a42ab4a1d8
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-31 15:08:37 -0300

    Added the US map (thru Turtle Module) to work on it

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("United States of America 'State Quizz' Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+
+screen.exitonclick()
